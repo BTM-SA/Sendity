96f27b63c1e7e4327a07152b7b24e27ed6203fcb
Cedric Bichet <299392493+C3671C@users.noreply.github.com>
2026-08-19 05:02:18 +0200

    Inject container into queue worker

--- app/Providers/QueueServiceProvider.php
+++ app/Providers/QueueServiceProvider.php
@@ -14,154 +14,83 @@ use Sendity\Queue\Retry\RetryPolicy;
 use Sendity\Queue\QueueStorageManager;
 use Sendity\Queue\Storage\FileQueueStorage;
 use Sendity\Queue\Drivers\File\FileQueueDriver;
-use Sendity\Queue\Contracts\QueueStorageInterface;
 
 class QueueServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        /*
-        |--------------------------------------------------------------------------
-        | Queue Driver Manager
-        |--------------------------------------------------------------------------
-        */
-
         $this->container->singleton(
             QueueDriverManager::class,
             function ($container) {
-
                 return new QueueDriverManager(
                     $container->get(Config::class),
                     $container
                 );
-
             }
         );
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Sync Queue Driver
-        |--------------------------------------------------------------------------
-        */
 
         $this->container->singleton(
             SyncQueueDriver::class,
             function () {
-
                 return new SyncQueueDriver();
-
             }
         );
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Retry Policy
-        |--------------------------------------------------------------------------
-        */
 
         $this->container->singleton(
             RetryPolicy::class,
             function () {
-
-                return new RetryPolicy(
-                    3
-                );
-
+                return new RetryPolicy(3);
             }
         );
 
-/*
-|--------------------------------------------------------------------------
-| Queue Storage Manager
-|--------------------------------------------------------------------------
-*/
-
-$this->container->singleton(
-    QueueStorageManager::class,
-    function ($container) {
-
-        return new QueueStorageManager(
-            $container->get(Config::class),
-            $container
+        $this->container->singleton(
+            QueueStorageManager::class,
+            function ($container) {
+                return new QueueStorageManager(
+                    $container->get(Config::class),
+                    $container
+                );
+            }
         );
 
-    }
-);
-
-
-/*
-|--------------------------------------------------------------------------
-| File Queue Storage
-|--------------------------------------------------------------------------
-*/
-
-$this->container->singleton(
-    FileQueueStorage::class,
-    function () {
-
-        return new FileQueueStorage();
-
-    }
-);
-        
-        /*
-|--------------------------------------------------------------------------
-| File Queue Driver
-|--------------------------------------------------------------------------
-*/
-
-$this->container->singleton(
-    FileQueueDriver::class,
-    function ($container) {
-
-        return new FileQueueDriver(
-            $container->get(
-                QueueStorageManager::class
-            )->storage()
+        $this->container->singleton(
+            FileQueueStorage::class,
+            function () {
+                return new FileQueueStorage();
+            }
         );
 
-    }
-);
-        
-        /*
-        |--------------------------------------------------------------------------
-        | Queue Manager
-        |--------------------------------------------------------------------------
-        */
+        $this->container->singleton(
+            FileQueueDriver::class,
+            function ($container) {
+                return new FileQueueDriver(
+                    $container->get(
+                        QueueStorageManager::class
+                    )->storage()
+                );
+            }
+        );
 
         $this->container->singleton(
             QueueManager::class,
             function ($container) {
-
                 return new QueueManager(
                     $container->get(QueueDriverManager::class)
                 );
-
             }
         );
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Queue Worker
-        |--------------------------------------------------------------------------
-        */
 
         $this->container->singleton(
             QueueWorker::class,
             function ($container) {
-
                 return new QueueWorker(
                     $container->get(QueueDriverManager::class),
-                    $container->get(RetryPolicy::class)
+                    $container->get(RetryPolicy::class),
+                    $container->get(\Sendity\Core\Container::class)
                 );
-
             }
         );
     }
-
 
     public function boot(): void
     {
